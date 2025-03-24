05426d58e1f629a4ce81d1598ed83bf739bae3ff
Byte-Master63 <thokozanetek@gmail.com>
2025-03-24 20:25:31 +0000

    README.md

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,10 @@
 from flask import Flask
-
-# Create a Flask application object
 app = Flask(__name__)
 
-# Define the route /airbnb-onepage/ and its corresponding view function
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
+@app.route('/airbnb-onepage/')
+def hello():
+    return 'Hello, AirBnB Clone!'
 
-# Run the Flask application on port 5000
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
